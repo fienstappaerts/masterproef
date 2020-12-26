989536e888a06f03317b19f8aa60960659326b67
Fien Stappaerts <fien.stappaerts@hotmail.com>
2020-12-26 12:21:50 +0100

    015-018

---
+++ experiments/016/sketch.js
@@ -0,0 +1,78 @@
+//I made a rectangle! Called R with translate length en rect as length*2 (line 55)
+
+let premise = 'R';
+  let rules = {
+    R: '[RRD]+DfFRFfR',
+  }
+let bigRule;
+
+
+let length = 90;
+let angle = 90;
+
+function setup() {
+  createCanvas(600, 600);
+  angleMode(DEGREES);
+  bigRule = resolveRules(premise, rules, 2);
+  print(bigRule);
+  
+}
+
+function draw() {
+  background('white');
+  stroke('black');
+  strokeWeight(4);
+  translate(width / 2, height / 2);
+  rotate(-90);
+  scale(0.3);
+
+  drawShape(bigRule);
+   // angle += 1;
+  // length += 0.1;
+}
+
+function resolveRules(startRule, rules, depth) {
+  let endRule = '';
+  for (let currentLetter of startRule) {
+    if (currentLetter in rules) {
+      endRule += rules[currentLetter];
+    } else {
+      endRule += currentLetter;
+    }
+  }
+  if (depth >= 1) {
+    endRule = resolveRules(endRule, rules, depth - 1);
+  }
+  return endRule;
+}
+
+
+function drawShape(rule)  {
+  for (let c of rule) {
+    if (c === 'F') {
+      line(0, 0, length, 0);
+      translate(length, 0);
+    } else if (c === 'R') {
+      rect(0, 0, length*2, length*2);
+      translate(length, length);
+    } else if (c === 'D') {
+      line(0, 0, length * 4, 0);
+      translate(length*4, 0);
+    } else if (c === 'f') {
+      translate(0, length, 0);
+    } else if (c === '+') {
+      rotate(angle);
+    } else if (c === '-') {
+      rotate(-angle);
+    } else if (c === '[') {
+      push();
+    } else if (c === ']') {
+      pop();
+    } else if (c === '>') {
+      scale(1.2);
+    } else if (c === '<') {
+      scale(0.8);
+   
+    }
+  }
+}
